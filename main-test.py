5d23ed7d77b5bf8f71959e4fee859e01f10a4286
ahmedsaad01123 <46676751+ahmedsaad01123@users.noreply.github.com>
2022-04-17 22:39:44 +0200

    Update main-test.py

--- main-test.py
+++ main-test.py
@@ -2,10 +2,14 @@
 from main import min_cost
 
 def test_min_cost():
-    cost = [[10, 20, 30, 40], [60, 50, 20, 80], [20, 20, 20, 20], [60, 50, 20, 80]]
+    cost = [[10, 20, 30, 40],
+            [60, 50, 20, 80],
+            [20, 20, 20, 20],
+            [60, 50, 20, 80]]
     m = len(cost)
     n = len(cost[0])
-    assert min(min_cost([[10, 20, 30, 40], [60, 50, 20, 80], [20, 20, 20, 20], [60, 50, 20, 80]], 3, 3)) == 80
+    min_cost_path = min_cost(cost, m-1, n-1)
+    assert (1000 - min(min_cost_path[n-1])) == 920
     print("min_cost Function works correctly")
 
 if __name__ == '__main__':
