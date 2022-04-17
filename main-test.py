fbb47a571451b2b371011a92dabb52ca7cd3b13d
ahmedsaad01123 <46676751+ahmedsaad01123@users.noreply.github.com>
2022-04-17 22:04:37 +0200

    add main
    add main

---
+++ main-test.py
@@ -0,0 +1,15 @@
+# Import the min_cost function, and assert that it works correctly.
+from main import min_cost
+
+def test_min_cost():
+    cost = [[10, 20, 30, 40],
+            [60, 50, 20, 80],
+            [20, 20, 20, 20],
+            [60, 50, 20, 80]]
+    m = len(cost)
+    n = len(cost[0])
+    assert min_cost(cost, m-1, n-1) == 80
+    print("min_cost Function works correctly")
+
+if __name__ == '__main__':
+        test_min_cost()
