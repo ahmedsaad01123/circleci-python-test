ca5cdf66635e9c8dbd48832cac4826b98a8af904
ahmedsaad01123 <46676751+ahmedsaad01123@users.noreply.github.com>
2022-04-18 17:04:20 +0200

    Update main-test.py

--- main-test.py
+++ main-test.py
@@ -9,7 +9,7 @@ def test_min_cost():
     m = len(cost)
     n = len(cost[0])
     min_cost_path = min_cost(cost, m-1, n-1)
-    assert (1000 - min(min_cost_path[n-1])) == 921
+    assert (1000 - min(min_cost_path[n-1])) == 920
     print("min_cost Function works correctly")
 
 if __name__ == '__main__':
